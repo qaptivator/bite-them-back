513c5174c8b20ed6f7af38c48b21c7c2dce9c390
qaptivator <ac@i-ns.net>
2024-10-03 18:51:32 +0300

    added armor specific animations

--- prototypes/entity/character.lua
+++ prototypes/entity/character.lua
@@ -1,5 +1,23 @@
+local biteranims = require("scripts.og.biter_animations")
+
 local char = data.raw["character"]["character"]
 local biter = data.raw["unit"]["small-biter"]
+
+small_biter_scale = 0.5
+small_biter_tint1 = { r = 0.60, g = 0.58, b = 0.51, a = 1 }
+small_biter_tint2 = { r = 0.9, g = 0.83, b = 0.54, a = 1 }
+
+medium_biter_scale = 0.7
+medium_biter_tint1 = { r = 0.49, g = 0.46, b = 0.51, a = 1 }
+medium_biter_tint2 = { r = 0.93, g = 0.72, b = 0.72, a = 1 }
+
+big_biter_scale = 1.0
+big_biter_tint1 = { r = 0.37, g = 0.40, b = 0.72, a = 1 }
+big_biter_tint2 = { r = 0.55, g = 0.76, b = 0.75, a = 1 }
+
+behemoth_biter_scale = 1.2
+behemoth_biter_tint1 = { r = 0.21, g = 0.19, b = 0.25, a = 1 }
+behemoth_biter_tint2 = { r = 0.657, g = 0.95, b = 0.432, a = 1.000 }
 
 -- biter animations have 16 directions
 --[[char.animations[1] = {
@@ -11,12 +29,49 @@ local biter = data.raw["unit"]["small-biter"]
 	mining_with_tool = biter.attack_parameters.animation, -- 8 directions
 }]]
 
-local anim = char.animations[1]
+-- all animations require 8 directions, except for running with gun for some odd reason
+local animations = {
+	{
+		idle = biteranims.biterrunanimation(small_biter_scale, small_biter_tint1, small_biter_tint2),
+		idle_with_gun = biteranims.biterrunanimation(small_biter_scale, small_biter_tint1, small_biter_tint2),
+		running = biteranims.biterrunanimation(small_biter_scale, small_biter_tint1, small_biter_tint2), -- TODO: decrease speed
+		mining_with_tool = biteranims.biterattackanimation(small_biter_scale, small_biter_tint1, small_biter_tint2),
+		running_with_gun = char.animations[1].running_with_gun, -- this requires 18 directions, while biter has 16. TODO: make a spritesheet for 18 directions
+		flipped_shadow_running_with_gun = char.animations[1].flipped_shadow_running_with_gun,
+	},
+	{
+		armors = { "heavy-armor" },
+		idle = biteranims.biterrunanimation(medium_biter_scale, medium_biter_tint1, medium_biter_tint2),
+		idle_with_gun = biteranims.biterrunanimation(medium_biter_scale, medium_biter_tint1, medium_biter_tint2),
+		running = biteranims.biterrunanimation(medium_biter_scale, medium_biter_tint1, medium_biter_tint2),
+		mining_with_tool = biteranims.biterattackanimation(medium_biter_scale, medium_biter_tint1, medium_biter_tint2),
+		running_with_gun = char.animations[1].running_with_gun,
+		flipped_shadow_running_with_gun = char.animations[1].flipped_shadow_running_with_gun,
+	},
+	{
+		armors = { "modular-armor", "power-armor" },
+		idle = biteranims.biterrunanimation(big_biter_scale, big_biter_tint1, big_biter_tint2),
+		idle_with_gun = biteranims.biterrunanimation(big_biter_scale, big_biter_tint1, big_biter_tint2),
+		running = biteranims.biterrunanimation(big_biter_scale, big_biter_tint1, big_biter_tint2),
+		mining_with_tool = biteranims.biterattackanimation(big_biter_scale, big_biter_tint1, big_biter_tint2),
+		running_with_gun = char.animations[1].running_with_gun,
+		flipped_shadow_running_with_gun = char.animations[1].flipped_shadow_running_with_gun,
+	},
+	{
+		armors = { "power-armor-mk2" },
+		idle = biteranims.biterrunanimation(behemoth_biter_scale, behemoth_biter_tint1, behemoth_biter_tint2),
+		idle_with_gun = biteranims.biterrunanimation(behemoth_biter_scale, behemoth_biter_tint1, behemoth_biter_tint2),
+		running = biteranims.biterrunanimation(behemoth_biter_scale, behemoth_biter_tint1, behemoth_biter_tint2),
+		mining_with_tool = biteranims.biterattackanimation(
+			behemoth_biter_scale,
+			behemoth_biter_tint1,
+			behemoth_biter_tint2
+		),
+		running_with_gun = char.animations[1].running_with_gun,
+		flipped_shadow_running_with_gun = char.animations[1].flipped_shadow_running_with_gun,
+	},
+}
 
-anim.running = biter.run_animation
-anim.idle = biter.run_animation
-anim.mining_with_tool = biter.attack_parameters.animation
-
-char.animations[1] = anim
+char.animations = animations
 
 data.raw["character"]["character"] = char
